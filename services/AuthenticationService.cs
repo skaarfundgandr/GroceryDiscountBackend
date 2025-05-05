8ce329817bfe26ecda60566cdede20668d2dc57e
skaarfungandr <mac2x.manos@gmail.com>
2025-05-06 02:58:51 +0800

    Add preliminary implementation of AuthenticationService and InventoryService

--- services/AuthenticationService.cs
+++ services/AuthenticationService.cs
@@ -1,1 +1,30 @@
 //TODO:
+using Microsoft.EntityFrameworkCore;
+using GROCERYDISCOUNTBACKEND.DATABASE;
+using GROCERYDISCOUNTBACKEND.DTO;
+
+namespace GROCERYDISCOUNTBACKEND.SERVICES {
+    public class AuthenticationService {
+        private readonly AppsdevDBContext _db;
+        public AuthenticationService(AppsdevDBContext db) => _db = db;
+
+        public async Task<bool> Authenticate(String username, String password) {
+            var user = await _db.Kiosks
+                .Where(res => res.Username == username)
+                .Where(res => res.Password == password)
+                .Select(res => new KioskDTO {
+                    Username = res.Username
+                })
+                .ToListAsync();
+            
+            if (user.Count > 1) {
+                throw new Exception("Duplicate entries found!");
+            } else if (user.Count == 0) {
+                return false;
+            } else {
+                Console.WriteLine($"{user[0].Username} Logged in");
+                return true;
+            }
+        }
+    }
+}
--- services/InventoryService.cs
+++ services/InventoryService.cs
@@ -1,1 +1,26 @@
-//TODO
+using Microsoft.EntityFrameworkCore;
+
+using GROCERYDISCOUNTBACKEND.DATABASE;
+using GROCERYDISCOUNTBACKEND.DTO;
+
+namespace GROCERYDISCOUNTBACKEND.SERVICES {
+    public class InventoryService {
+        private readonly AppsdevDBContext _db;
+
+        public InventoryService(AppsdevDBContext db) => _db = db;
+
+        public async Task<List<InventoryProductsViewDTO> GetInventoryProductsAsync() {
+            return await _db.InventoryProducts
+                .Select(res => new InventoryProductsViewDTO {
+                    ProductName = res.ProductName,
+                    Description = res.Description,
+                    Category = res.Category,
+                    Price = res.Price,
+                    Quantity = res.Quantity,
+                    Units = res.Units,
+                    LastRestock = res.LastRestock
+                })
+                .ToListAsync();
+        }
+    }
+}
